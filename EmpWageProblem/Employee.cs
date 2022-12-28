5c11d083f19e45a1b62b819353a8821edd0afb2d
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-28 21:42:11 +0530

    [SaiVenkat]-Add: Calculate Employee Monthly Wage

--- EmpWageProblem/Employee.cs
+++ EmpWageProblem/Employee.cs
@@ -8,32 +8,46 @@ namespace EmpWageProblem
 {
     public class Employee
     {
-        public const int IS_FULL_TIME = 1, IS_PART_TIME = 2, Rate_Per_Hour = 20;
-        int empHrs = 0, EmpDailyWage = 0;
-
+        public const int IS_FULL_TIME = 1, IS_PART_TIME = 2, Rate_Per_Hour = 20, Total_Working_Days = 20;
+        
         public void CheckAttendance()
         {
-            
+            int empHrs = 0, MonthlyWage = 0, totalHrs=0;
+
             Random random = new Random();
             int empCheck = random.Next(0, 3);
-
-            switch (empCheck)
+            if (empCheck == IS_FULL_TIME)
             {
-                case IS_PART_TIME:
-                    Console.WriteLine("Employee is Part Time");
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    Console.WriteLine("Employee is Full Time");
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    Console.WriteLine("Employee is Absent");
-                    break;
+                Console.WriteLine("Employee is Full Time");
             }
-            EmpDailyWage = Rate_Per_Hour * empHrs;
-            Console.WriteLine("Employee Wage is " + EmpDailyWage);
+            else if (empCheck == IS_PART_TIME)
+            {
+                Console.WriteLine("Employee is Part Time");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
+            for (int day = 0; day < Total_Working_Days; day++)
+            {
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                      
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalHrs += empHrs;
+            }
+            MonthlyWage = Rate_Per_Hour * totalHrs;
+            Console.WriteLine("Employee Wage is " + MonthlyWage);
 
         }
     }
