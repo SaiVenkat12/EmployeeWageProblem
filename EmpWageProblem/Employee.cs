9b6a8d793ea1631b90bda61a34487965f1359152
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-28 21:29:01 +0530

    [SaiVenkat]-Add: Employee Wage using Switch Case

--- EmpWageProblem/Employee.cs
+++ EmpWageProblem/Employee.cs
@@ -8,7 +8,8 @@ namespace EmpWageProblem
 {
     public class Employee
     {
-        int IS_FULL_TIME = 1, Is_PART_TIME = 2, Rate_Per_Hour = 20, empHrs = 0, EmpDailyWage = 0;
+        public const int IS_FULL_TIME = 1, IS_PART_TIME = 2, Rate_Per_Hour = 20;
+        int empHrs = 0, EmpDailyWage = 0;
 
         public void CheckAttendance()
         {
@@ -16,19 +17,20 @@ namespace EmpWageProblem
             Random random = new Random();
             int empCheck = random.Next(0, 3);
 
-            if (empCheck == IS_FULL_TIME)
+            switch (empCheck)
             {
-                Console.WriteLine("Employee is Full Time");
-                empHrs = 8;
-            }
-            else if (empCheck == Is_PART_TIME)
-            {
-                Console.WriteLine("Employee is Part Time");
-                empHrs = 4;
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
+                case IS_PART_TIME:
+                    Console.WriteLine("Employee is Part Time");
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    Console.WriteLine("Employee is Full Time");
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    Console.WriteLine("Employee is Absent");
+                    break;
             }
             EmpDailyWage = Rate_Per_Hour * empHrs;
             Console.WriteLine("Employee Wage is " + EmpDailyWage);
